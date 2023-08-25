2e3075681ca6b0467e16e825dd7b7d4de1170f36
Chase Manning <chase@manning.dev>
2023-08-25 15:30:26 +0100

    :rocket: create extension

---
+++ scripts/content.js
@@ -0,0 +1,101 @@
+const FIXER_API_KEY = "b667423fdaded850df7b1efd1f1c022b";
+
+const getPrices = async (currencies) => {
+  const url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_T0mPlEkWPwoUnPAT6R6fC4V18UibBnTEeVvfcqzK&currencies=${currencies.join(
+    ","
+  )}&base_currency=NZD`;
+  const request = await fetch(url);
+  const response = await request.json();
+  return response.data;
+};
+
+let gettingPrices = false;
+
+let checkElementInterval = setInterval(async () => {
+  const overallContainer = document.getElementsByClassName("balance-1-1-1");
+
+  if (!overallContainer) {
+    console.log("BNZBankBuddy: No overall container found");
+    return;
+  }
+
+  const overall = overallContainer[0].querySelector(".js-net-pos");
+  if (!overall) {
+    console.log("BNZBankBuddy: No overall found");
+    return;
+  }
+
+  const tooltip = overallContainer[0].querySelector(".help-1-1-3");
+
+  // Getting prices
+  if (gettingPrices) return;
+  gettingPrices = true;
+  const accountBalances = document.getElementsByClassName(`account-balance`);
+  let currencies = [];
+  for (let i = 0; i < accountBalances.length; i++) {
+    const accountBalance = accountBalances[i];
+    const accountBalanceText = accountBalance.textContent;
+    if (accountBalanceText.includes(" ")) {
+      const firstNumberIndex = accountBalanceText.search(/\d/);
+      const firstLetterIndex = accountBalanceText.search(/[a-zA-Z]/);
+      const currency = accountBalanceText.substring(
+        firstLetterIndex,
+        firstNumberIndex - 1
+      );
+      currencies.push(currency);
+    }
+  }
+  const prices = await getPrices(currencies);
+
+  // Get account total
+  let newTotal = 0;
+  if (!accountBalances) {
+    console.log("BNZBankBuddy: No account balances found");
+    return;
+  }
+  for (let i = 0; i < accountBalances.length; i++) {
+    const accountBalance = accountBalances[i];
+    const accountBalanceText = accountBalance.textContent;
+
+    // Handling foreign currency
+    if (accountBalanceText.includes(" ")) {
+      const firstNumberIndex = accountBalanceText.search(/\d/);
+      const firstLetterIndex = accountBalanceText.search(/[a-zA-Z]/);
+      const currency = accountBalanceText.substring(
+        firstLetterIndex,
+        firstNumberIndex - 1
+      );
+      const price = prices[currency];
+      const accountBalanceNumber = parseFloat(
+        accountBalanceText
+          .substring(firstNumberIndex, accountBalanceText.length - 1)
+          .replace(/,/g, "")
+      );
+      newTotal += accountBalanceNumber / price;
+      console.log(
+        `BNZBankBuddy: ${currency} Account found`,
+        (accountBalanceNumber / price).toLocaleString("en-US")
+      );
+    }
+    // Handling NZD
+    else {
+      const accountBalanceNumber = parseFloat(
+        accountBalanceText.replace(/,/g, "")
+      );
+      newTotal += accountBalanceNumber;
+      console.log(
+        "BNZBankBuddy: Account found",
+        accountBalanceNumber.toLocaleString("en-US")
+      );
+    }
+  }
+
+  // Remove tooltip
+  if (tooltip) {
+    tooltip.remove();
+  }
+
+  // Update overall
+  overall.textContent = newTotal.toLocaleString("en-US");
+  clearInterval(checkElementInterval);
+}, 1000);
