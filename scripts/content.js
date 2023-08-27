c30e408e83644d1b8f2e5d46118e9f51e61660c9
Chase Manning <chase@manning.dev>
2023-08-27 03:44:00 +0100

    :art: code improvements

--- scripts/content.js
+++ scripts/content.js
@@ -1,97 +1,67 @@
+// Config
+const URL = "https://api.freecurrencyapi.com/v1/latest";
+const API_KEY = "fca_live_T0mPlEkWPwoUnPAT6R6fC4V18UibBnTEeVvfcqzK";
+const INTERVAL = 1000; // 1 second
+
+// Gets prices for a given array of currencies
 const getPrices = async (currencies) => {
-  const url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_T0mPlEkWPwoUnPAT6R6fC4V18UibBnTEeVvfcqzK&currencies=${currencies.join(
-    ","
-  )}&base_currency=NZD`;
+  const cur = currencies.join(",");
+  const url = `${URL}?apikey=${API_KEY}&currencies=${cur}&base_currency=NZD`;
   const request = await fetch(url);
   const response = await request.json();
   return response.data;
 };
 
+const getBalance = (balance) => {
+  // FX account
+  if (balance.includes(" ")) {
+    const end = balance.search(/\d/);
+    const start = balance.search(/[a-zA-Z]/);
+    const number = balance.substring(end, balance.length - 1).replace(/,/g, "");
+    return {
+      currency: balance.substring(start, end - 1),
+      amount: parseFloat(number),
+    };
+  }
+
+  // NZD account
+  return {
+    currency: "NZD",
+    amount: parseFloat(balance.replace(/,/g, "")),
+  };
+};
+
 let checkElementInterval = setInterval(async () => {
+  // Getting elements
   const overallContainer = document.getElementsByClassName("balance-1-1-1");
-
-  if (!overallContainer) {
-    console.log("BNZBankBuddy: No overall container found");
-    return;
-  }
-
+  if (!overallContainer || !overallContainer[0]) return;
   const overall = overallContainer[0].querySelector(".js-net-pos");
-  if (!overall) {
-    console.log("BNZBankBuddy: No overall found");
-    return;
-  }
+  if (!overall) return;
+  const accountBalances = document.getElementsByClassName(`account-balance`);
+  if (!accountBalances) return;
   clearInterval(checkElementInterval);
 
-  const tooltip = overallContainer[0].querySelector(".help-1-1-3");
-
   // Getting prices
-  const accountBalances = document.getElementsByClassName(`account-balance`);
-  let currencies = [];
+  const currencies = [];
   for (let i = 0; i < accountBalances.length; i++) {
-    const accountBalance = accountBalances[i];
-    const accountBalanceText = accountBalance.textContent;
-    if (accountBalanceText.includes(" ")) {
-      const firstNumberIndex = accountBalanceText.search(/\d/);
-      const firstLetterIndex = accountBalanceText.search(/[a-zA-Z]/);
-      const currency = accountBalanceText.substring(
-        firstLetterIndex,
-        firstNumberIndex - 1
-      );
-      currencies.push(currency);
-    }
+    let balance = getBalance(accountBalances[i].textContent);
+    if (balance.currency !== "NZD") currencies.push(balance.currency);
   }
   const prices = await getPrices(currencies);
 
   // Get account total
   let newTotal = 0;
-  if (!accountBalances) {
-    console.log("BNZBankBuddy: No account balances found");
-    return;
-  }
   for (let i = 0; i < accountBalances.length; i++) {
-    const accountBalance = accountBalances[i];
-    const accountBalanceText = accountBalance.textContent;
-
-    // Handling foreign currency
-    if (accountBalanceText.includes(" ")) {
-      const firstNumberIndex = accountBalanceText.search(/\d/);
-      const firstLetterIndex = accountBalanceText.search(/[a-zA-Z]/);
-      const currency = accountBalanceText.substring(
-        firstLetterIndex,
-        firstNumberIndex - 1
-      );
-      const price = prices[currency];
-      const accountBalanceNumber = parseFloat(
-        accountBalanceText
-          .substring(firstNumberIndex, accountBalanceText.length - 1)
-          .replace(/,/g, "")
-      );
-      newTotal += accountBalanceNumber / price;
-      console.log(
-        `BNZBankBuddy: ${currency} Account found`,
-        (accountBalanceNumber / price).toLocaleString("en-US")
-      );
-    }
-    // Handling NZD
-    else {
-      const accountBalanceNumber = parseFloat(
-        accountBalanceText.replace(/,/g, "")
-      );
-      newTotal += accountBalanceNumber;
-      console.log(
-        "BNZBankBuddy: Account found",
-        accountBalanceNumber.toLocaleString("en-US")
-      );
-    }
+    let balance = getBalance(accountBalances[i].textContent);
+    if (balance.currency !== "NZD") balance.amount /= prices[balance.currency];
+    newTotal += balance.amount;
   }
 
   // Remove tooltip
-  if (tooltip) {
-    tooltip.remove();
-  }
+  const tooltip = overallContainer[0].querySelector(".help-1-1-3");
+  if (tooltip) tooltip.remove();
 
   // Update overall
-
   newTotal = Math.round(newTotal * 100) / 100;
   overall.textContent = newTotal.toLocaleString("en-US");
-}, 1000);
+}, INTERVAL);
